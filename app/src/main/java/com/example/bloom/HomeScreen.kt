7b63ab91505796e20a7e0d5db22417ec9a5c964e
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-04-11 23:22:11 +0430

    create homeGardenThemes part 1

--- app/src/main/java/com/example/bloom/HomeScreen.kt
+++ app/src/main/java/com/example/bloom/HomeScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,9 +31,38 @@ fun HomeScreen() {
             Spacer(modifier = Modifier.height(40.dp))
             SearchInput()
             BroseThemesSection()
-
+            HomeGardenSection()
 
         }
+    }
+}
+
+@Composable
+private fun HomeGardenSection() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            text = "Design your home garden",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier
+                .paddingFromBaseline(
+                    top = 40.dp,
+                    bottom = 16.dp
+                )
+                .weight(1F)
+
+        )
+        Icon(
+            Icons.Default.List,
+            contentDescription = "Filter",
+            modifier = Modifier
+                .size(24.dp)
+
+
+        )
     }
 }
 
--- app/src/main/java/com/example/bloom/PlantTheme.kt
+++ app/src/main/java/com/example/bloom/PlantTheme.kt
@@ -2,16 +2,24 @@ package com.example.bloom
 
 
 data class PlantTheme(
-    val imageRes:Int,
-    val title:String,
+    val imageRes: Int,
+    val title: String,
 )
 
-val defaultPlantTheme= listOf(
-    PlantTheme(R.drawable.desert_chic,"Desert chic"),
-    PlantTheme(R.drawable.tiny_terrariums,"Tiny terrariums"),
-    PlantTheme(R.drawable.jungle_vibes,"Jungle vibes"),
-    PlantTheme(R.drawable.easy_care,"Easy care"),
-    PlantTheme(R.drawable.statements,"Statements"),
+val defaultPlantTheme = listOf(
+    PlantTheme(R.drawable.desert_chic, "Desert chic"),
+    PlantTheme(R.drawable.tiny_terrariums, "Tiny terrariums"),
+    PlantTheme(R.drawable.jungle_vibes, "Jungle vibes"),
+    PlantTheme(R.drawable.easy_care, "Easy care"),
+    PlantTheme(R.drawable.statements, "Statements"),
+)
+val homeGardenThemes = listOf(
+    PlantTheme(R.drawable.monstera,"Monstera"),
+    PlantTheme(R.drawable.aglaonema,"Aglaonema"),
+    PlantTheme(R.drawable.peace_lily,"Peace lily"),
+    PlantTheme(R.drawable.fiddle_leaf,"Fiddle leaf tree"),
+    PlantTheme(R.drawable.snake_plant,"Snake plant"),
+    PlantTheme(R.drawable.pothos,"Pothos"),
 )
 
 
